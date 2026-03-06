7089325d0d4d06d9b9213c0d9c748f84f3779219
Mohammad Muksith <mohammadmuksith04@gmail.com>
2026-03-06 17:46:43 -0500

    new commits

--- FleetTracker/FleetTracker/ViewModels/LocationManager.swift
+++ FleetTracker/FleetTracker/ViewModels/LocationManager.swift
@@ -54,30 +54,4 @@ final class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObje
     }
 }
 
-//class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
-//    let manager = CLLocationManager()
-//
-//    @Published var location: CLLocationCoordinate2D?
-//
-//    override init() {
-//        super.init()
-//        manager.delegate = self
-//        manager.requestWhenInUseAuthorization()
-//    }
-//
-//    func requestLocation() {
-//        manager.requestLocation()
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager,
-//                         didUpdateLocations locations: [CLLocation]) {
-//        location = locations.first?.coordinate
-//    }
-//
-//    func locationManager(_ manager: CLLocationManager,
-//                         didFailWithError error: Error) {
-//        print("Location error:", error.localizedDescription)
-//    }
-//}
-
 
