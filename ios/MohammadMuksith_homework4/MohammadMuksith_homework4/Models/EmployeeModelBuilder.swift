c766131e7f49d22f2f945d9fd492ef8cc0477da2
Mohammad Muksith <mohammadmuksith04@gmail.com>
2026-03-06 18:51:44 -0500

    Model Builder

--- ios/MohammadMuksith_homework4/MohammadMuksith_homework4/Models/EmployeeModelBuilder.swift
+++ ios/MohammadMuksith_homework4/MohammadMuksith_homework4/Models/EmployeeModelBuilder.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+final class EmployeeModelBuilder {
+    private var name: String = ""
+    private var latitudes: [String]    = []
+    private var longitudes: [String]   = []  
+    private var locationTimes: [String] = []
+
+    @discardableResult
+    func setName(_ name: String) -> EmployeeModelBuilder {
+        self.name = name   
+        return self
+    }
+
+    @discardableResult
+    func addLocation(lat: String, lng: String, time: String) -> EmployeeModelBuilder {
+        latitudes.append(lat)
+        longitudes.append(lng)
+        locationTimes.append(time)
+        return self
+    }
+
+    @discardableResult
+    func reset() -> EmployeeModelBuilder {
+        name = ""; latitudes = []; longitudes = []; locationTimes = []
+        return self
+    }
+
+    func build() -> EmployeeModel {
+        precondition(!name.isEmpty, "Name must be set before calling build().")
+        return EmployeeModel(name: name,
+                             latitude: latitudes,
+                             longitude: longitudes,
+                             locationTime: locationTimes)
+    }
+}
+
