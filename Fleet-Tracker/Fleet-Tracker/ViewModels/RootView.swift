a425c19e716c296e33d52db642f1e6e41a3da433
Maher2732 <Maher.Yousif69@gmail.com>
2026-03-11 22:21:48 -0400

    brought over files from 2 branches

---
+++ Fleet-Tracker/Fleet-Tracker/ViewModels/RootView.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+import FirebaseAuth
+
+struct RootView: View {
+    @StateObject private var viewModel = SignInViewModel()
+
+    var body: some View {
+        Group {
+            if viewModel.user != nil {
+                NavigationStack {
+                    MapViewWrapper()
+                        .toolbar {
+                            ToolbarItem(placement: .topBarTrailing) {
+                                Button("Logout") {
+                                    viewModel.signOut()
+                                }
+                            }
+                        }
+                }
+            } else {
+                LoginView(viewModel: viewModel)
+            }
+        }
+    }
+}
