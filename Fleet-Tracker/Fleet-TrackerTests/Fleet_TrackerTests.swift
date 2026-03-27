8c0440c4081247ae7d5b8cafd1ecd2b6c2d2852b
Maher2732 <Maher.Yousif69@gmail.com>
2026-03-26 21:51:02 -0400

    Added user tests

---
+++ Fleet-Tracker/Fleet-TrackerTests/Fleet_TrackerTests.swift
@@ -0,0 +1,74 @@
+import XCTest
+@testable import Fleet_Tracker
+
+final class PhotoUploadViewModelTests: XCTestCase {
+
+    var viewModel: PhotoUploadViewModel!    
+
+    override func setUp() {
+        super.setUp()
+        viewModel = PhotoUploadViewModel()
+    }
+
+    override func tearDown() {
+        viewModel = nil
+        super.tearDown()
+    }
+
+    func testHasSelectedImage_WhenNoImage_ReturnsFalse() {
+        viewModel.selectedImage = nil
+        XCTAssertFalse(viewModel.hasSelectedImage(), "Should return false when no image is selected")
+    }
+
+    func testHasSelectedImage_WhenImageSet_ReturnsTrue() {
+        viewModel.selectedImage = UIImage(systemName: "star")
+        XCTAssertTrue(viewModel.hasSelectedImage(), "Should return true when an image is selected")
+    }
+
+    func testResetState_ClearsAllValues() {
+        viewModel.isUploading = true
+        viewModel.uploadSuccess = true
+        viewModel.errorMessage = "Some error"
+        viewModel.selectedImage = UIImage(systemName: "star")
+
+        viewModel.resetState()
+
+        XCTAssertFalse(viewModel.isUploading)
+        XCTAssertFalse(viewModel.uploadSuccess)
+        XCTAssertNil(viewModel.errorMessage)
+        XCTAssertNil(viewModel.selectedImage)
+    }
+
+    func testSetError_SetsMessageAndStopsUploading() {
+        viewModel.isUploading = true
+        viewModel.setError("Upload failed")
+
+        XCTAssertEqual(viewModel.errorMessage, "Upload failed")
+        XCTAssertFalse(viewModel.isUploading)
+        XCTAssertFalse(viewModel.uploadSuccess)
+    }
+
+    func testSetSuccess_ClearsImageAndMarksSuccess() {
+        viewModel.selectedImage = UIImage(systemName: "star")
+        viewModel.isUploading = true
+
+        viewModel.setSuccess()
+
+        XCTAssertTrue(viewModel.uploadSuccess)
+        XCTAssertFalse(viewModel.isUploading)
+        XCTAssertNil(viewModel.selectedImage)
+    }
+
+    func testGenerateFilename_ContainsUIDAndJpgExtension() {
+        let uid = "testUser123"
+        let filename = viewModel.generateFilename(uid: uid)
+
+        XCTAssertTrue(filename.hasPrefix(uid), "Filename should start with the UID")
+        XCTAssertTrue(filename.hasSuffix(".jpg"), "Filename should end with .jpg")
+    }
+
+    func testIsValidImage_WithValidImage_ReturnsTrue() {
+        let image = UIImage(systemName: "star")!
+        XCTAssertTrue(viewModel.isValidImage(image))
+    }
+}
